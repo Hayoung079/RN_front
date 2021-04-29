e0ad91ca82b878e33b23f57224425fd6b99d18ca
Hayoung079 <gksyddl22@gamil.com>
2021-04-29 13:07:04 +0900

    react nativebase 설치 및 링크

--- Screen/DrawerScreens/SettingsScreen.js
+++ Screen/DrawerScreens/SettingsScreen.js
@@ -1,17 +1,22 @@
 // Import React and Component
 import React from 'react';
-import {View, Text, StyleSheets} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const SettingsScreen = () => {
     return (    
         <View>
             <View>
-                <Text>개인 설정</Text>
+                <TouchableOpacity>
+                    <Text>개인 설정</Text>
+                </TouchableOpacity>
             </View>
             <View>
-                <Text>비밀번호 변경</Text>
+                <TouchableOpacity>
+                    <Text>비밀번호 변경</Text>
+                </TouchableOpacity>
             </View>
             <View>
                 <Text>지갑 만들기</Text>
